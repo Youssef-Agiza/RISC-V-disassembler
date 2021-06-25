68f1042954b52dc7459942d44d9a3e0d7ba9af4d
Youssef Agiza <youssefagiza@aucegypt.edu>
2021-06-26 00:23:45 +0200

    implements B-type instructions

--- src/rvcdiss-youssef.cpp
+++ src/rvcdiss-youssef.cpp
@@ -30,7 +30,7 @@ using namespace std;
 
 unsigned int pc = 0x0;
 
-char memory[8*1024];	// only 8KB of memory located at address 0
+char memory[8 * 1024]; // only 8KB of memory located at address 0
 
 void emitError(char *s)
 {
@@ -38,7 +38,8 @@ void emitError(char *s)
 	exit(0);
 }
 
-void printPrefix(unsigned int instA, unsigned int instW){
+void printPrefix(unsigned int instA, unsigned int instW)
+{
 	cout << "0x" << hex << std::setfill('0') << std::setw(8) << instA << "\t0x" << std::setw(8) << instW;
 }
 
@@ -59,59 +60,115 @@ void instDecExec(unsigned int instWord)
 	// — inst[31] — inst[30:25] inst[24:21] inst[20]
 	I_imm = ((instWord >> 20) & 0x7FF) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
 
+	// — inst[31] — inst[7] inst[30:25] inst[11:8] 0
+
+	B_imm = (((instWord >> 8) & 0xF) << 1) |
+			(((instWord >> 25) & 0x3F) << 5) |
+			(((instWord >> 7) & 0x1) << 10) |
+			((instWord >> 31) ? 0xFFFFF800 : 0x0);
+
 	printPrefix(instPC, instWord);
 
-	if(opcode == 0x33){		// R Instructions
-		switch(funct3){
-			case 0: if(funct7 == 32) {
-								cout << "\tSUB\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
-							}
-							else {
-								cout << "\tADD\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
-							}
-							break;
-			default:
-							cout << "\tUnkown R Instruction \n";
+	if (opcode == 0x33)
+	{ // R Instructions
+		switch (funct3)
+		{
+		case 0:
+			if (funct7 == 32)
+			{
+				cout << "\tSUB\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+			}
+			else
+			{
+				cout << "\tADD\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+			}
+			break;
+		default:
+			cout << "\tUnkown R Instruction \n";
 		}
-	} else if(opcode == 0x13){	// I instructions
-		switch(funct3){
-			case 0:	cout << "\tADDI\tx" << rd << ", x" << rs1 << ", " << hex << "0x" << (int)I_imm << "\n";
-					break;
-			default:
-					cout << "\tUnkown I Instruction \n";
+	}
+	else if (opcode == 0x13)
+	{ // I instructions
+		switch (funct3)
+		{
+		case 0:
+			cout << "\tADDI\tx" << rd << ", x" << rs1 << ", " << hex << "0x" << (int)I_imm << "\n";
+			break;
+		default:
+			cout << "\tUnkown I Instruction \n";
 		}
-	} else {
+	}
+	else if (opcode == 0x63)
+	{ //B-Type
+
+		switch (funct3)
+		{
+		case 0:
+			std::cout << "\tBEQ\tx" << rs1 << ", x" << rs2 << ", " << std::hex << "0x" << (int)B_imm << "\n";
+			break; //BEQ
+		case 1:
+			std::cout << "\tBNE\tx" << rs1 << ", x" << rs2 << ", " << std::hex << "0x" << (int)B_imm << "\n";
+
+			break; //BNE
+		case 4:
+			std::cout << "\tBLT\tx" << rs1 << ", x" << rs2 << ", " << std::hex << "0x" << (int)B_imm << "\n";
+
+			break; //BLT
+		case 5:
+			std::cout << "\tBGT\tx" << rs1 << ", x" << rs2 << ", " << std::hex << "0x" << (int)B_imm << "\n";
+
+			break; //BGE
+		case 6:
+			std::cout << "\tBLTU\tx" << rs1 << ", x" << rs2 << ", " << std::hex << "0x" << (int)B_imm << "\n";
+
+			break; //BLTU
+		case 7:
+			std::cout << "\tBGTU\tx" << rs1 << ", x" << rs2 << ", " << std::hex << "0x" << (int)B_imm << "\n";
+
+			break; //BGEU
+		default:
+			std::cout << "\tUnkown B Instruction \n";
+		}
+	}
+	else
+	{
 		cout << "\tUnkown Instruction \n";
 	}
-
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
-	unsigned int instWord=0;
+	unsigned int instWord = 0;
 	ifstream inFile;
 	ofstream outFile;
 
-	if(argc<2) emitError("use: rvcdiss <machine_code_file_name>\n");
+	if (argc < 2)
+		emitError((char *)"use: rvcdiss <machine_code_file_name>\n");
 
 	inFile.open(argv[1], ios::in | ios::binary | ios::ate);
 
-	if(inFile.is_open())
+	if (inFile.is_open())
 	{
 		int fsize = inFile.tellg();
 
-		inFile.seekg (0, inFile.beg);
-		if(!inFile.read((char *)memory, fsize)) emitError("Cannot read from input file\n");
+		inFile.seekg(0, inFile.beg);
+		if (!inFile.read((char *)memory, fsize))
+			emitError((char *)"Cannot read from input file\n");
 
-		while(true){
-				instWord = 	(unsigned char)memory[pc] |
-							(((unsigned char)memory[pc+1])<<8) |
-							(((unsigned char)memory[pc+2])<<16) |
-							(((unsigned char)memory[pc+3])<<24);
-				pc += 4;
-				// remove the following line once you have a complete simulator
-				if(pc==40) break;			// stop when PC reached address 32
-				instDecExec(instWord);
+		while (true)
+		{
+			instWord = (unsigned char)memory[pc] |
+					   (((unsigned char)memory[pc + 1]) << 8) |
+					   (((unsigned char)memory[pc + 2]) << 16) |
+					   (((unsigned char)memory[pc + 3]) << 24);
+			pc += 4;
+			// remove the following line once you have a complete simulator
+			if (pc == 40)
+				break; // stop when PC reached address 32
+			instDecExec(instWord);
 		}
-	} else emitError("Cannot access input file\n");
+	}
+	else
+		emitError((char *)"Cannot access input file\n");
 }

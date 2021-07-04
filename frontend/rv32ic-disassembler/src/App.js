9947673b30ed7d983656b4d609afd214a084b7d5
Youssef Agiza <youssefagiza@aucegypt.edu>
2021-07-05 01:10:36 +0200

    inital step in the website

--- frontend/rv32ic-disassembler/src/App.js
+++ frontend/rv32ic-disassembler/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/footer";
+import { useState } from "react";
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  // On file select (from the pop up)
+  const onFileChange = (event) => {
+    // Update the state
+    setSelectedFile(event.target.files[0]);
+  };
+
+  // On file upload (click the upload button)
+  const onFileUpload = () => {
+    const formData = new FormData();
+    formData.append("myFile", selectedFile, selectedFile.name);
+
+    console.log(selectedFile);
+
+    // Request made to the backend api
+    // Send formData object
+    // axios.post("api/uploadfile", formData);
+  };
+
+  // File content to be displayed after
+  // file upload is complete
+  const fileData = () => {
+    if (selectedFile) {
+      return (
+        <div className="m-4">
+          <h2>File Details:</h2>
+          <p>File Name: {selectedFile.name}</p>
+          <p>File Type: {selectedFile.type}</p>
+          <p>
+            Last Modified:
+            {selectedFile.lastModifiedDate.toDateString()}
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
+      );
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-container">
+      <main className="container content-wrapper">
+        <h1>RISC-V Disassembler</h1>
+        <div className="m-4">
+          <input type="file" onChange={onFileChange} />
+          <button onClick={onFileUpload}>Upload!</button>
+        </div>
+        {fileData()}
+      </main>
+      {/* <Footer /> */}
     </div>
   );
 }
